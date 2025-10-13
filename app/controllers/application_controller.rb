823e9f271fa29bc9b8940b5fa8d080524a77dba4
西山和秀 <nishiyamakazuhide@nishiyamakazuhidenoMacBook-Air.local>
2025-10-13 19:56:26 +0900

    ログインの有無でtop.htmlのボタン表示とページ遷移の変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,12 @@
 class ApplicationController < ActionController::Base
-  
+
+  # CSRF攻撃対策
+  protect_from_forgery with: :exception
+
+  # カスタム認証を使用
+  before_action :custom_authenticate_user!, unless: :public_page?
+
+  # deviseコントローラーにストロングパラメータを追加する  
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -7,5 +14,20 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+  # カスタム認証メソッド
+  def custom_authenticate_user!
+    return if user_signed_in?
+    redirect_to new_user_session_path, alert: 'ログインが必要です。'
+  end
+
+  private
+
+  # 公開ページ（ログイン不要）の判定
+  def public_page?
+  # ルートページとDeviseページは公開
+    (controller_name == 'static_pages' && action_name == 'top') ||
+    devise_controller?
   end
 end
