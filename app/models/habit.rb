aa7eefb91f5cf042065688b1ce16bd9258c5c787
西山和秀 <nishiyamakazuhide@nishiyamakazuhidenoMacBook-Air.local>
2025-10-19 16:35:03 +0900

    habitモデル,

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,3 +1,6 @@
 class Habit < ApplicationRecord
   belongs_to :user
+  has_many :daily_habit_records, dependent: :destroy
+
+  validates :title, presence: true
 end
--- db/migrate/20251019073021_create_habits.rb
+++ db/migrate/20251019073021_create_habits.rb
@@ -2,8 +2,8 @@ class CreateHabits < ActiveRecord::Migration[7.2]
   def change
     create_table :habits do |t|
       t.references :user, null: false, foreign_key: true
-      t.string :title
-      t.boolean :is_default
+      t.string :title, null: false
+      t.boolean :is_default, default: false, null: false
 
       t.timestamps
     end
