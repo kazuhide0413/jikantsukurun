d3f1af01aae2138c7226163875444f8e7dde559b
西山和秀 <nishiyamakazuhide@nishiyamakazuhidenoMacBook-Air.local>
2025-10-25 21:57:04 +0900

    user.rbにモデル追記、habits_controller追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :habits, dependent: :destroy
+  has_many :daily_habit_records, through: :habits
+
   validates :name, presence: true
 
 end
