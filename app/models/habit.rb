d02db6d82a46e500fa0284fb50a282f6b3efca8c
西山和秀 <nishiyamakazuhide@nishiyamakazuhidenoMacBook-Air.local>
2025-10-26 18:48:25 +0900

    同じ習慣が登録されないように修正

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -3,6 +3,19 @@ class Habit < ApplicationRecord
   has_many :daily_habit_records, dependent: :destroy
 
   validates :title, presence: true
+  validates :title, uniqueness: { scope: :user_id, message: "はすでに登録されています" }
+  validate :title_cannot_duplicate_default
 
   scope :default_habits, -> { where(is_default: true) }
+
+  private
+
+  # デフォルト習慣と同名タイトルは禁止
+  def title_cannot_duplicate_default
+    if user_id.present? && Habit.default_habits.exists?(title: title)
+      # 👇 メッセージを統一（Rails標準のuniquenessと同じ）
+      errors.add(:title, "はすでに登録されています")
+    end
+  end
+
 end
