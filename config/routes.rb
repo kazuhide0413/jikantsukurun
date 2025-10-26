17c79dc54b29af5575d414b18c703347305a959c
西山和秀 <nishiyamakazuhide@nishiyamakazuhidenoMacBook-Air.local>
2025-10-26 19:25:55 +0900

    ルート記載

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   ## メイン機能
   root "static_pages#top"
   resources :habits
+  resource :settings, only: [:show, :update]
 
   ## 静的ページ
   get "guide", to: "guide#index"
