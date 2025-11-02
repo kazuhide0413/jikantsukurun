491eb81c87b9197894f0b0935d00d563e3a42baf
西山和秀 <nishiyamakazuhide@nishiyamakazuhidenoMacBook-Air.local>
2025-11-02 17:08:42 +0900

    habits_controllerの位置、余白を削除

--- app/controllers/habits_controller.rb
+++ app/controllers/habits_controller.rb
@@ -2,21 +2,19 @@ class HabitsController < ApplicationController
   before_action :authenticate_user!  # Devise使用時
   before_action :set_habit, only: [:show, :edit, :update, :destroy]
 
-def index
-  @habits = Habit.where(user_id: [current_user.id, nil])
-  @today_session = current_user.daily_sessions.find_or_create_by(session_date: Date.current)
+  def index
+    @habits = Habit.where(user_id: [current_user.id, nil])
+    @today_session = current_user.daily_sessions.find_or_create_by(session_date: Date.current)
 
-  today = Date.current
+    today = Date.current
 
-  # ✅ 現在のユーザーの習慣に対応する記録（user_idは固定しない）
-  records = DailyHabitRecord
-              .where(record_date: today, habit_id: @habits.pluck(:id), is_completed: true)
-              .pluck(:habit_id)
+    # ✅ 現在のユーザーの習慣に対応する記録（user_idは固定しない）
+    records = DailyHabitRecord
+                .where(record_date: today, habit_id: @habits.pluck(:id), is_completed: true)
+                .pluck(:habit_id)
 
-  @completed_habit_ids = records
-end
-
-
+    @completed_habit_ids = records
+  end
 
   def show
     @habit = Habit.where(user_id: [current_user.id, nil]).find(params[:id])
