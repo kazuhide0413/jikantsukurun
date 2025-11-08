30e6c585c4ca21ac90065e9fd2d18b5722c3c931
西山和秀 <nishiyamakazuhide@nishiyamakazuhidenoMacBook-Air.local>
2025-11-08 13:32:38 +0900

    Habitsテーブルのis_booleanカラム削除（マイグレーション実行）

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_11_08_041118) do
+ActiveRecord::Schema[7.2].define(version: 2025_11_08_042953) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -49,7 +49,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_11_08_041118) do
   create_table "habits", force: :cascade do |t|
     t.bigint "user_id"
     t.string "title", null: false
-    t.boolean "is_default", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_habits_on_user_id"
