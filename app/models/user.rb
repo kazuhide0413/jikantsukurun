d56fe85a0f9eb8ad59a00765ee1445a5beebc33d
西山和秀 <nishiyamakazuhide@nishiyamakazuhidenoMacBook-Air.local>
2026-01-08 00:35:25 +0900

    routes.rb,user.rb編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :habits, dependent: :destroy
   has_many :daily_habit_records, through: :habits
