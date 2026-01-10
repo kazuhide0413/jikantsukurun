fcb37e5d2c03f34e1e2d072d48e4c94f50ed1576
西山和秀 <nishiyamakazuhide@nishiyamakazuhidenoMacBook-Air.local>
2026-01-10 13:21:46 +0900

    user.rb編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,16 +8,28 @@ class User < ApplicationRecord
   has_many :daily_sessions, dependent: :destroy
 
   validates :name, presence: true
+  validates :uid, uniqueness: { scope: :provider }, if: -> { uid.present? }
 
   after_create :copy_default_habits
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.name = auth.info.name.presence || auth.info.email
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.avatar_url = auth.info.image if user.respond_to?(:avatar_url) && auth.info.image
+    end
+  end
+
+  def self.create_unique_string
+    SecureRandom.uuid
+  end
 
   private
 
   def copy_default_habits
     DefaultHabit.find_each do |template|
-      habits.create!(
-        title: template.title
-      )
+      habits.create!(title: template.title)
     end
   end
 end
