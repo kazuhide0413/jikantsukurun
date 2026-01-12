6ee97bccde99de7ccb9ab7d7de44822cae687778
西山和秀 <nishiyamakazuhide@nishiyamakazuhidenoMacBook-Air.local>
2026-01-12 15:54:42 +0900

    live_webhook_controllerにskip_before_action :custom_authenticate_user!, raise: falseを追加

--- app/controllers/line_webhook_controller.rb
+++ app/controllers/line_webhook_controller.rb
@@ -1,5 +1,6 @@
 class LineWebhookController < ApplicationController
   skip_before_action :verify_authenticity_token
+  skip_before_action :custom_authenticate_user!, raise: false
 
   def create
     body = request.raw_post
