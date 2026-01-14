1ffef3aaa82fcc161c2c1d96cdb64a9af780def8
西山和秀 <nishiyamakazuhide@nishiyamakazuhidenoMacBook-Air.local>
2026-01-14 22:33:03 +0900

    Usersにline_link_tokenを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_14_132502) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_14_133106) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,7 +67,9 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_14_132502) do
     t.string "uid"
     t.string "avatar_url"
     t.string "line_messaging_user_id"
+    t.string "line_link_token"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["line_link_token"], name: "index_users_on_line_link_token", unique: true
     t.index ["line_messaging_user_id"], name: "index_users_on_line_messaging_user_id", unique: true
     t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
