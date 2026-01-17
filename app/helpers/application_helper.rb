772f411b324d83262a9f14e29e60d1084506df74
西山和秀 <nishiyamakazuhide@nishiyamakazuhidenoMacBook-Air.local>
2026-01-17 18:13:11 +0900

    OGP導入

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,28 @@
+module ApplicationHelper
+  def default_meta_tags
+    {
+      site: '時間作るん',
+      title: '習慣化サービス',
+      reverse: true,
+      charset: 'utf-8',
+      description: '時間作るんは、誰もが毎日必ずする習慣を手助けするサービスです',
+      keywords: '習慣化',
+      canonical: https://jikantsukurun.onrender.com,
+      separator: '|',
+      og:{
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: https://jikantsukurun.onrender.com,
+        image: image_url('ogp.png'),
+        local: 'ja-JP'
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@obvyamdrss',
+        image: image_url('ogp.png')
+      }
+    }
+  end
+end
