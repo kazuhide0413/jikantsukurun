066c6e5655fda95a399ae893e21dd09cc6fcda7d
西山和秀 <nishiyamakazuhide@nishiyamakazuhidenoMacBook-Air.local>
2026-01-20 23:27:02 +0900

    system_testを修正

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,13 @@
 require_relative "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 390, 844 ]
+  driven_by :selenium, using: :headless_chrome, screen_size: [ 390, 844 ], options: {
+    browser: :chrome,
+    args: %w[
+      no-sandbox
+      disable-dev-shm-usage
+      disable-gpu
+      window-size=390,844
+    ]
+  }
 end
