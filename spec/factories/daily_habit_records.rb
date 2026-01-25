220417a21106949cc696f6332fa0bb65e6de45ce
西山和秀 <nishiyamakazuhide@nishiyamakazuhidenoMacBook-Air.local>
2026-01-25 19:08:29 +0900

    dailyhabitrecordのモデルspecを実装

--- spec/factories/daily_habit_records.rb
+++ spec/factories/daily_habit_records.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :daily_habit_record do
     association :user
-    association :habit
     record_date { Date.current }
     is_completed { false }
     completed_at { nil }
+
+    habit { association :habit, user: user }
   end
 end
